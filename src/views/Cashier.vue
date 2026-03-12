0b40fca0264be5e2ac2b6a8a79f833094833d6d3
Kent Anthony <126381769+kentillation@users.noreply.github.com>
2026-03-12 15:54:23 +0800

    Update Cashier.vue

--- src/views/Cashier.vue
+++ src/views/Cashier.vue
@@ -116,7 +116,7 @@
             <!-- Order Sheet -->
             <v-bottom-sheet v-model="selectedOrderDialog">
                 <v-card style="background-color: #e8faff;">
-                    <v-stepper v-model="orderStep" class="pb-4 modern-stepper" elevation="0" alt-labels>
+                    <!-- <v-stepper v-model="orderStep" class="pb-4 modern-stepper" elevation="0" alt-labels>
                         <v-stepper-header>
                             <v-stepper-item title="Order" :value="1" complete></v-stepper-item>
                             <v-divider></v-divider>
@@ -124,7 +124,7 @@
                             <v-divider></v-divider>
                             <v-stepper-item title="Place order" :value="3"></v-stepper-item>
                         </v-stepper-header>
-                    </v-stepper>
+                    </v-stepper> -->
                     
                     <v-container class="overflow-auto pb-10" style="height: 700px;">
                         <!-- Orders -->
