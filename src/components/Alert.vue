4321852621267a9ddae20035108cf427674fc503
Kent Anthony <126381769+kentillation@users.noreply.github.com>
2026-03-13 14:26:33 +0800

    Update Alert.vue

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -2,9 +2,9 @@
 <template>
     <v-snackbar v-model="snackbar.visible" :color="snackbar.color" location="top" class="mt-5" timeout="5000"
       persistent>
-      <div class="d-flex align-items-center">
-        <span style="font-size: 13px;"><v-icon icon="mdi-information-outline"></v-icon></span>
-        <span style="font-size: 13px;">&nbsp; {{ snackbar.message }}</span>
+      <div class="d-flex align-center">
+        <span style="font-size: 14px;"><v-icon icon="mdi-information-outline"></v-icon></span>
+        <span style="font-size: 14px;">&nbsp; {{ snackbar.message }}</span>
       </div>
     </v-snackbar>
 </template>
