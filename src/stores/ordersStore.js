86f40b85e80b0bb79cc1ea2cebd2ca1a66824ee3
Kent Anthony Engbino <kentanthony070495@gmail.com>
2026-05-25 15:11:37 +0800

    Updated store

--- src/stores/ordersStore.js
+++ src/stores/ordersStore.js
@@ -3,13 +3,9 @@ import { defineStore } from 'pinia';
 export const useOrdersStore = defineStore('orders', {
     state: () => ({
         crrntTtlOrdrQntty: 0,
-        crrntSbTtl: 0,
-        crrntTtlAmnt: 0,
     }),
 
     getters: {
         currentTotalOrderQuantity: (state) => state.crrntTtlOrdrQntty,
-        currentSubTotal: (state) => state.crrntSbTtl,
-        currentTotalAmount: (state) => state.crrntTtlAmnt,
     },
 });
